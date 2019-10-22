6bda357ffb8b82d1324cf4e3f9b12a32dae1f248
Ashleigh Barker <barkerashleigh1@gmail.com>
2019-10-21 22:33:05 -0500

    More Changes

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,8 +6,75 @@ function start (){
     for(let i = 0; i < drawBoard.length; i++)
     {
         drawBoard[i].setAttribute("class", "square");
-        //console.log(drawBoard[i]);
     }
-    //console.log(drawBoard);
+    //const playArray = new Array();
+    play_game();
+    //console.log(playArray);
 }
 window.onload = start;
+
+
+
+/*function play(){
+    let makePlay = document.getElementById("board").querySelectorAll('.square');
+    for(let i = 0; i < makePlay.length; i++)
+    {
+        if(playArray.length === 0)
+        {
+            makePlay[i].innerHTML = "X";
+            makePlay[i].setAttribute("class","X");
+            playArray.push('X');
+        }
+        else
+        {
+            if(playArray[playArray.length-1] === 'X')
+            {
+                makePlay[i].innerHTML = "O";
+                makePlay[i].setAttribute("class","O");
+                playArray.push('O');
+            }
+            else if(playArray[playArray.length-1] === 'O')
+            {
+                makePlay[i].innerHTML = "X";
+                makePlay[i].setAttribute("class","X");
+                playArray.push('X');
+            }
+        }
+    }
+}*/
+let playArray = new Array();
+function play_game()
+{
+    let makePlay = document.querySelectorAll('.square');
+    for(let i = 0; i < makePlay.length; i++)
+    {
+        makePlay[i].onclick = function(){
+            if(playArray.length === 0)
+            {
+                makePlay[i].classList.add("square","X");
+                makePlay[i].innerHTML = "X";
+                playArray.push('X');
+            }
+            else
+            {
+                if(playArray[playArray.length-1] === 'X')
+                {
+                    makePlay[i].classList.add("square","O");
+                    makePlay[i].innerHTML = "O";
+                    //makePlay[i].setAttribute("class","O");
+                    playArray.push('O');
+                }
+                else if(playArray[playArray.length-1] === 'O')
+                {
+                    makePlay[i].classList.add("square","X");
+                    makePlay[i].innerHTML = "X";
+                   // makePlay[i].setAttribute("class","X");
+                    playArray.push('X');
+                }
+            }
+        }
+    }
+}
+
+//play_game();
+console.log(playArray);
