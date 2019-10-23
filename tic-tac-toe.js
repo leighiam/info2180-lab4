03a34d3dff444c4264f7faca99ec4d8fb1cd6758
Ashleigh Barker <barkerashleigh1@gmail.com>
2019-10-22 20:18:58 -0500

    One more change

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,11 +6,13 @@ function start (){
     for(let i = 0; i < drawBoard.length; i++)
     {
         drawBoard[i].setAttribute("class", "square");
+        //console.log(drawBoard[i].innerHTML);
     }
     //const playArray = new Array();
     play_game();
     active_square();
-    //console.log(playArray);
+   // winner();
+    
 }
 window.onload = start;
 
@@ -44,7 +46,7 @@ function play_game()
                    // makePlay[i].setAttribute("class","X");
                     playArray.push('X');
                 }
-            }
+            }winner();
         }
     }
 }
@@ -54,7 +56,6 @@ function active_square()
     let current_square = document.querySelectorAll('.square');
     for(let i = 0; i < current_square.length; i++)
     {
-        
         current_square[i].addEventListener("mouseover", function()
         {
             current_square[i].classList.add("hover");
@@ -63,6 +64,102 @@ function active_square()
         {
             current_square[i].classList.remove("hover");
         });
+    }
+}
+
+function winner()
+{
+    let game_Board = document.getElementById("board").children;
+    let square1 = game_Board[0].innerHTML;
+    let square2 = game_Board[1].innerHTML;
+    let square3 = game_Board[2].innerHTML;
+    let square4 = game_Board[3].innerHTML;
+    let square5 = game_Board[4].innerHTML;
+    let square6 = game_Board[5].innerHTML;
+    let square7 = game_Board[6].innerHTML;
+    let square8 = game_Board[7].innerHTML;
+    let square9 = game_Board[8].innerHTML;
+    let status_msg = document.getElementById("status");
+    if(square1 === "X"  && square2 === "X" && square3 === "X")
+    {
+      
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! X is the Winner!";
+    }
+    else if(square4 === "X"  && square5 === "X" && square6 === "X")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! X is the Winner!";
+    }
+    else if(square7 === "X"  && square8 === "X" && square9 === "X")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! X is the Winner!";
+    }
+    else if(square1 === "X"  && square4 === "X" && square7 === "X")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! X is the Winner!";
+    }
+    else if(square2 === "X"  && square5 === "X" && square8 === "X")
+    { 
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! X is the Winner!";
+    }
+    else if(square3 === "X"  && square6 === "X" && square9 === "X")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! X is the Winner!";
+    }
+    else if(square1 === "X"  && square5 === "X" && square9 === "X")
+    { 
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! X is the Winner!";
+    }
+    else if(square3 === "X"  && square5 === "X" && square7 === "X")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! X is the Winner!";
+    }
+    else if(square1 === "O"  && square2 === "O" && square3 === "O")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! O is the Winner!";
+    }
+    else if(square4 === "O"  && square5 === "O" && square6 === "O")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! O is the Winner!";
+    }
+    else if(square7 === "O"  && square8 === "O" && square9 === "O")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! O is the Winner!";
+    }
+    else if(square1 === "O"  && square4 === "O" && square7 === "O")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! O is the Winner!";
+    }
+    else if(square2 === "O"  && square5 === "O" && square8 === "O")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! O is the Winner!";
+    }
+    else if(square3 === "O"  && square6 === "O" && square9 === "O")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! O is the Winner!";
+    }
+    else if(square1 === "O"  && square5 === "O" && square9 === "O")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! O is the Winner!";
+    }
+    else if(square3 === "O"  && square5 === "O" && square7 === "O")
+    {
+        status_msg.classList.add("you-won");
+        status_msg.innerHTML = "Congratulations! O is the Winner!";
     }
 }
 
